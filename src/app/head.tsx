22b92c065bb929e883963e108b48cfc097da0b0c
Moh abbh <musst92@gmail.com>
2025-07-14 23:59:30 +0300

    vire

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
         <>
             {/* Google الجوجل */}
             <meta name="google-site-verification" content="sWFdmLk4UeLp9-e8_x4jOgFJHnRiCbvb0aEBpi9_3tw" />
-
+        
             {/* اللغة والترميز */}
             <meta charSet="UTF-8" />
             <meta httpEquiv="Content-Language" content="ar" />
