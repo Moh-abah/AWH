c7202bf348947d36ff229105aa70700d18ffcc21
Moh abbh <musst92@gmail.com>
2025-07-15 01:50:29 +0300

    imagsalah

--- src/components/about/TeamSection.tsx
+++ src/components/about/TeamSection.tsx
@@ -13,6 +13,11 @@ const teamMembers = [
         photo: '/images/teams/alisuliman.jpg',
     },
     {
+        name: ' صلاح القاره',
+        role: ' مطور الواجهه الاماميه   ',
+        photo: '/images/teams/salah.jpg',
+    },
+    {
         name: ' محمد الشرعبي    ',
         role: ' مطور الواجهه الاماميه    ',
         photo: '/images/teams/sharabi.jpg',
@@ -45,7 +50,7 @@ export default function TeamSection() {
                 {teamMembers.map(({ name, role, photo }) => (
                     <div
                         key={name}
-                        className="bg-white rounded-xl shadow-md p-6 text-center transition-transform transform hover:scale-105 hover:shadow-xl cursor-pointer"
+                        className="bg-whi   te rounded-xl shadow-md p-6 text-center transition-transform transform hover:scale-105 hover:shadow-xl cursor-pointer"
                     >
                         <div className="w-32 h-32 mx-auto rounded-full overflow-hidden mb-4 border-4 border-blue-500">
                             <Image
