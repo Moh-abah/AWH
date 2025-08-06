9791cccfac786adcebc3d1e94e19853ebb71d856
Moh abbh <musst92@gmail.com>
2025-08-06 19:08:49 +0300

    analiytc

--- src/app/contact/clintcontact.tsx
+++ src/app/contact/clintcontact.tsx
@@ -54,8 +54,8 @@ export default function ContactPage() {
             }
     
             const currentPage = window.location.pathname + window.location.search;
-            let pages = localStorage.getItem('visited_pages');
-            let pagesArray = pages ? JSON.parse(pages) : [];
+            const pages = localStorage.getItem('visited_pages');
+            const pagesArray = pages ? JSON.parse(pages) : [];
             if (!pagesArray.includes(currentPage)) {
                 pagesArray.push(currentPage);
                 localStorage.setItem('visited_pages', JSON.stringify(pagesArray));
